cb4d7892552f6563dcc536de303e0beff197b531
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-09-22 10:49:49 +0530

    Login Screen and profile screen dynamically updated

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,119 +1,246 @@
 import {
+  ActivityIndicator,
+  Alert,
   Image,
+  KeyboardAvoidingView,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { LockClosedIcon, EnvelopeIcon } from "react-native-heroicons/outline";
 import { useNavigation } from "@react-navigation/native";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { themeColor } from "../theme/themeColors";
 
 export default function LoginScreen() {
   const navigation = useNavigation();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const userId = auth.currentUser?.uid;
+
+  useEffect(() => {
+    setLoading(true);
+    const unsubscribe = onAuthStateChanged(auth, async (authUser) => {
+      if (!authUser) {
+        setLoading(false);
+      }
+      if (authUser) {
+        const docRef = doc(db, "users", `${authUser.uid}`);
+
+        try {
+          const docSnapshot = await getDoc(docRef);
+
+          if (docSnapshot.exists()) {
+            const sub = docSnapshot.data();
+            // console.log(sub);
+            if (sub.bio && sub.accountId) {
+              navigation.navigate("Main");
+            } else {
+              navigation.navigate("ProfileSetup");
+            }
+          } else {
+            console.log("Document does not exist");
+          }
+        } catch (error) {
+          console.log("Error");
+        }
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const loginUser = async () => {
+    if (email === "" || password === "") {
+      Alert.alert("Invalid Details", "Please fill all the details to login", [
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+        {
+          text: "Ok",
+          style: "default",
+        },
+      ]);
+    } else {
+      signInWithEmailAndPassword(auth, email, password)
+        .then(async (userCredential) => {
+          const user = userCredential.user;
+          Alert.alert(
+            "Login Success",
+            "Your account has been logged in successfully. Enjoy!",
+            [
+              {
+                text: "cancel",
+                style: "cancel",
+              },
+              {
+                text: "ok",
+                style: "default",
+              },
+            ],
+            { cancelable: true }
+          );
+          const docSnap = await getDoc(doc(db, "users", `${userId}`));
+
+          if (docSnap.exists()) {
+            console.log(docSnap.data());
+          }
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          Alert.alert("Error", errorMessage, [
+            {
+              text: "Ok",
+              style: "cancel",
+            },
+          ]);
+        });
+    }
+  };
 
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
-        paddingHorizontal: 30,
-        gap: 30,
+        alignItems: "center",
         backgroundColor: "white",
       }}
     >
-      <View style={{ alignItems: "center" }}>
-        <Image
-          source={{
-            uri: "https://freelogopng.com/images/all_img/1688663386threads-logo-transparent.png",
-          }}
-          style={{
-            width: 80,
-            height: 80,
-            objectFit: "contain",
-          }}
-        />
-      </View>
+      {loading ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <View style={{ alignItems: "center" }}>
+            <Image
+              source={require("../assets/images/loadingLogo.png")}
+              style={{
+                width: 80,
+                height: 80,
+                objectFit: "contain",
+              }}
+            />
+          </View>
+          <ActivityIndicator
+            style={{ marginTop: 30 }}
+            size={"large"}
+            color={themeColor.primaryColor}
+          />
+        </View>
+      ) : (
+        <KeyboardAvoidingView style={{ flex: 1 }}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: "center",
+              paddingHorizontal: 30,
+              gap: 30,
+              backgroundColor: "white",
+            }}
+          >
+            <View style={{ alignItems: "center" }}>
+              <Image
+                source={require("../assets/images/tLogo.png")}
+                style={{
+                  width: 80,
+                  height: 80,
+                  objectFit: "contain",
+                }}
+              />
+            </View>
 
-      <View>
-        <Text style={{ textAlign: "center", fontWeight: 700, fontSize: 18 }}>
-          Login to your account
-        </Text>
-      </View>
+            <View>
+              <Text
+                style={{ textAlign: "center", fontWeight: 700, fontSize: 18 }}
+              >
+                Login to your account
+              </Text>
+            </View>
 
-      <View
-        style={{
-          borderWidth: 1,
-          borderRadius: 10,
-          paddingVertical: 20,
-          flexDirection: "row",
-          paddingHorizontal: 30,
-          alignItems: "center",
-          gap: 10,
-        }}
-      >
-        <EnvelopeIcon size={25} color={"black"} />
-        <TextInput
-          placeholder="Email Address"
-          autoCapitalize="none"
-          autoCorrect={false}
-          onChangeText={(text) => setEmail(text)}
-        />
-      </View>
-      <View
-        style={{
-          borderWidth: 1,
-          borderRadius: 10,
-          paddingVertical: 20,
-          flexDirection: "row",
-          alignItems: "center",
-          paddingHorizontal: 30,
-          gap: 10,
-        }}
-      >
-        <LockClosedIcon size={25} color={"black"} />
-        <TextInput
-          placeholder="Password"
-          secureTextEntry
-          autoCorrect={false}
-          onChangeText={(text) => setPassword(text)}
-        />
-      </View>
-      <View
-        style={{
-          borderWidth: 1,
-          borderRadius: 10,
-          paddingVertical: 20,
-          flexDirection: "row",
-          paddingHorizontal: 30,
-          gap: 10,
-          alignItems: "center",
-          backgroundColor: "black",
-          justifyContent: "center",
-        }}
-      >
-        <Text style={{ color: "white", fontWeight: 600, fontSize: 16 }}>
-          Login
-        </Text>
-      </View>
-      <View
-        style={{
-          flexDirection: "row",
-          gap: 10,
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <Text style={{ color: "black", fontWeight: 500, fontSize: 17 }}>
-          Don't have an account?
-        </Text>
-        <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-          <Text style={{ fontWeight: 700, fontSize: 17 }}>Register Now</Text>
-        </TouchableOpacity>
-      </View>
+            <View
+              style={{
+                borderWidth: 1,
+                borderRadius: 10,
+                paddingVertical: 20,
+                flexDirection: "row",
+                paddingHorizontal: 30,
+                alignItems: "center",
+                gap: 10,
+              }}
+            >
+              <EnvelopeIcon size={25} color={"black"} />
+              <TextInput
+                placeholder="Email Address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                onChangeText={(text) => setEmail(text)}
+              />
+            </View>
+            <View
+              style={{
+                borderWidth: 1,
+                borderRadius: 10,
+                paddingVertical: 20,
+                flexDirection: "row",
+                alignItems: "center",
+                paddingHorizontal: 30,
+                gap: 10,
+              }}
+            >
+              <LockClosedIcon size={25} color={"black"} />
+              <TextInput
+                placeholder="Password"
+                secureTextEntry
+                autoCorrect={false}
+                onChangeText={(text) => setPassword(text)}
+              />
+            </View>
+            <TouchableOpacity
+              onPress={loginUser}
+              style={{
+                borderWidth: 1,
+                borderRadius: 10,
+                paddingVertical: 20,
+                flexDirection: "row",
+                paddingHorizontal: 30,
+                gap: 10,
+                alignItems: "center",
+                backgroundColor: "black",
+                justifyContent: "center",
+              }}
+            >
+              <Text style={{ color: "white", fontWeight: 600, fontSize: 16 }}>
+                Login
+              </Text>
+            </TouchableOpacity>
+            <View
+              style={{
+                flexDirection: "row",
+                gap: 10,
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ color: "black", fontWeight: 500, fontSize: 17 }}>
+                Don't have an account?
+              </Text>
+              <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+                <Text style={{ fontWeight: 700, fontSize: 17 }}>
+                  Register Now
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </KeyboardAvoidingView>
+      )}
     </View>
   );
 }
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,4 +1,6 @@
 import {
+  ActivityIndicator,
+  Alert,
   Image,
   ScrollView,
   StyleSheet,
@@ -6,144 +8,203 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   LockClosedIcon,
   Bars3BottomRightIcon,
+  ArrowRightOnRectangleIcon,
 } from "react-native-heroicons/outline";
 import { AntDesign } from "@expo/vector-icons";
+import { signOut } from "firebase/auth";
+import { auth, db } from "../firebase";
+import { useNavigation } from "@react-navigation/native";
+import { doc, getDoc } from "firebase/firestore";
+import { themeColor } from "../theme/themeColors";
 
 export default function ProfileScreen() {
+  const navigation = useNavigation();
+  const currentUser = auth.currentUser.uid;
+  const [profileData, setProfileData] = useState(null);
+
+  useEffect(() => {
+    const getProfileData = async () => {
+      const docRef = doc(db, "users", `${currentUser}`);
+
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const profileInformation = docSnap.data();
+        setProfileData(profileInformation);
+      }
+    };
+
+    getProfileData();
+  }, []);
+
+  const signOutUser = () => {
+    signOut(auth)
+      .then(() => {
+        navigation.replace("Login");
+      })
+      .catch((error) => {
+        Alert.alert("Error", error, [
+          {
+            text: "ok",
+            style: "default",
+          },
+          {
+            text: "cancel",
+            style: "cancel",
+          },
+        ]);
+      });
+  };
   return (
     <View style={{ flex: 1, backgroundColor: "white", paddingHorizontal: 20 }}>
-      <ScrollView>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <View>
-            <LockClosedIcon color={"black"} size={30} />
-          </View>
+      {profileData ? (
+        <ScrollView>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <View>
+              <LockClosedIcon color={"black"} size={30} />
+            </View>
 
-          <View style={{ flexDirection: "row", gap: 10, alignItems: "center" }}>
-            <AntDesign name="instagram" size={30} color="black" />
-            <Bars3BottomRightIcon color={"black"} size={35} />
-          </View>
-        </View>
-        {/* Profile Informarion */}
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginTop: 20,
-          }}
-        >
-          <View style={{ gap: 5, marginTop: 15 }}>
-            <Text style={{ fontWeight: 600 }}>Username</Text>
             <View
-              style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
+              style={{ flexDirection: "row", gap: 10, alignItems: "center" }}
             >
-              <Text style={{ fontWeight: 600, fontSize: 16 }}>
-                vinay_._sandhu
-              </Text>
-              <TouchableOpacity
-                style={{
-                  backgroundColor: "#dfdfdf",
-                  paddingHorizontal: 8,
-                  borderRadius: 20,
-                  paddingVertical: 4,
-                }}
-              >
-                <Text style={{ fontSize: 12 }}>threads.net</Text>
+              <AntDesign name="instagram" size={30} color="black" />
+              <TouchableOpacity onPress={signOutUser}>
+                <ArrowRightOnRectangleIcon color={"black"} size={30} />
               </TouchableOpacity>
             </View>
-            <Text style={{ fontWeight: 600 }}>Expectation</Text>
-            <View
-              style={{
-                flexDirection: "row",
-                gap: 5,
-                alignItems: "center",
-                marginTop: 5,
-              }}
-            >
+          </View>
+          {/* Profile Informarion */}
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginTop: 30,
+            }}
+          >
+            <View style={{ gap: 5, marginTop: 15 }}>
+              <Text style={{ fontWeight: 600 }}>{profileData?.username}</Text>
               <View
-                style={{ flexDirection: "row", gap: -5, alignItems: "center" }}
+                style={{ flexDirection: "row", alignItems: "center", gap: 8 }}
               >
-                <Image
-                  source={{
-                    uri: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60",
-                  }}
+                <Text style={{ fontWeight: 600, fontSize: 16 }}>
+                  vinay_._sandhu
+                </Text>
+                <TouchableOpacity
                   style={{
-                    width: 25,
-                    height: 25,
-                    objectFit: "cover",
-                    borderRadius: 50,
+                    backgroundColor: "#dfdfdf",
+                    paddingHorizontal: 8,
+                    borderRadius: 20,
+                    paddingVertical: 4,
                   }}
-                />
-                <Image
-                  source={{
-                    uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60",
-                  }}
-                  style={{
-                    width: 25,
-                    height: 25,
-                    objectFit: "cover",
-                    borderRadius: 50,
-                  }}
-                />
+                >
+                  <Text style={{ fontSize: 12 }}>threads.net</Text>
+                </TouchableOpacity>
               </View>
-              <Text style={{ fontWeight: 500 }}>8 followers</Text>
+              <Text style={{ fontWeight: 600 }}>Expectation</Text>
+              <View
+                style={{
+                  flexDirection: "row",
+                  gap: 5,
+                  alignItems: "center",
+                  marginTop: 5,
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    gap: -5,
+                    alignItems: "center",
+                  }}
+                >
+                  <Image
+                    source={{
+                      uri: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60",
+                    }}
+                    style={{
+                      width: 25,
+                      height: 25,
+                      objectFit: "cover",
+                      borderRadius: 50,
+                    }}
+                  />
+                  <Image
+                    source={{
+                      uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60",
+                    }}
+                    style={{
+                      width: 25,
+                      height: 25,
+                      objectFit: "cover",
+                      borderRadius: 50,
+                    }}
+                  />
+                </View>
+                <Text style={{ fontWeight: 500 }}>8 followers</Text>
+              </View>
+            </View>
+            <View>
+              <Image
+                source={{
+                  uri: profileData.profileImage,
+                }}
+                style={{
+                  width: 80,
+                  height: 80,
+                  objectFit: "cover",
+                  borderRadius: 50,
+                }}
+              />
             </View>
           </View>
-          <View>
-            <Image
-              source={{
-                uri: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60",
-              }}
+          <View
+            style={{
+              marginTop: 15,
+              flexDirection: "row",
+              justifyContent: "center",
+              gap: 18,
+            }}
+          >
+            <TouchableOpacity
               style={{
-                width: 80,
-                height: 80,
-                objectFit: "cover",
-                borderRadius: 50,
+                borderWidth: 1,
+                borderColor: "#a3a3a3",
+                paddingVertical: 10,
+                borderRadius: 10,
+                paddingHorizontal: 50,
               }}
-            />
+            >
+              <Text>Edit Profile</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                borderWidth: 1,
+                borderColor: "#a3a3a3",
+                paddingVertical: 10,
+                borderRadius: 10,
+                paddingHorizontal: 50,
+              }}
+            >
+              <Text>Share Profile</Text>
+            </TouchableOpacity>
           </View>
-        </View>
+        </ScrollView>
+      ) : (
         <View
-          style={{
-            marginTop: 15,
-            flexDirection: "row",
-            justifyContent: "center",
-            gap: 18,
-          }}
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
-          <TouchableOpacity
-            style={{
-              borderWidth: 1,
-              borderColor: "#a3a3a3",
-              paddingVertical: 10,
-              borderRadius: 10,
-              paddingHorizontal: 50,
-            }}
-          >
-            <Text>Edit Profile</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              borderWidth: 1,
-              borderColor: "#a3a3a3",
-              paddingVertical: 10,
-              borderRadius: 10,
-              paddingHorizontal: 50,
-            }}
-          >
-            <Text>Share Profile</Text>
-          </TouchableOpacity>
+          <ActivityIndicator size={"large"} color={themeColor.primaryColor} />
         </View>
-      </ScrollView>
+      )}
     </View>
   );
 }
