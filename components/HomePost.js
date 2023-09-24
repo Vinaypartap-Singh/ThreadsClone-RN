adfca1cac32baa36308e03d6e2f959942687cf3b
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-09-25 05:10:53 +0530

    Home Post component layout fixes

--- components/HomePost.js
+++ components/HomePost.js
@@ -16,126 +16,160 @@ import {
 } from "react-native-heroicons/outline";
 import { AntDesign } from "@expo/vector-icons";
 
-export default function HomePost() {
+export default function HomePost({ data }) {
+  const profileInfo = data.userProfile;
   return (
-    <View style={{ marginVertical: 20, marginHorizontal: 10, height: "auto" }}>
-      <View
-        style={{
-          flexDirection: "row",
-          //   alignItems: "center",
-          //   backgroundColor: "red",
-          paddingHorizontal: 20,
-        }}
-      >
-        <View style={{ alignItems: "center", height: 270 }}>
-          <Image
-            source={{
-              uri: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3560&q=80",
-            }}
-            style={{
-              width: 60,
-              height: 60,
-              objectFit: "cover",
-              borderRadius: 100,
-            }}
-          />
-          <View style={{ alignItems: "center" }}>
-            <View
-              style={{ borderWidth: 1, height: "68%", marginTop: 5 }}
-            ></View>
-            {/* bottom Images Stack */}
-            <View
-              style={{ flexDirection: "row", alignItems: "center", gap: 4 }}
-            >
-              <Image
-                source={{
-                  uri: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3560&q=80",
-                }}
+    <>
+      {data?.threads ? (
+        <View
+          style={{
+            marginVertical: 20,
+            marginHorizontal: 10,
+            height: "auto",
+            gap: 50,
+          }}
+        >
+          {data.threads.map((data, index) => {
+            return (
+              <View
+                key={index}
                 style={{
-                  width: 20,
-                  height: 20,
-                  borderRadius: 100,
-                  marginTop: -10,
+                  flexDirection: "row",
+                  //   alignItems: "center",
+                  //   backgroundColor: "red",
+                  paddingHorizontal: 20,
                 }}
-              />
-              <Image
-                source={{
-                  uri: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3560&q=80",
-                }}
-                style={{
-                  width: 20,
-                  height: 20,
-                  borderRadius: 100,
-                  marginTop: 10,
-                }}
-              />
-              <Image
-                source={{
-                  uri: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3560&q=80",
-                }}
-                style={{
-                  width: 20,
-                  height: 20,
-                  borderRadius: 100,
-                  marginTop: -10,
-                }}
-              />
-            </View>
-          </View>
+              >
+                <View
+                  style={{
+                    alignItems: "center",
+                    height: "auto",
+                  }}
+                >
+                  <Image
+                    source={{
+                      uri: profileInfo.profileImage,
+                    }}
+                    style={{
+                      width: 60,
+                      height: 60,
+                      objectFit: "cover",
+                      borderRadius: 100,
+                    }}
+                  />
+                  <View style={{ alignItems: "center" }}>
+                    <View
+                      style={{ borderWidth: 1, height: 40, marginTop: 5 }}
+                    ></View>
+                    {/* bottom Images Stack */}
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                        gap: 4,
+                      }}
+                    >
+                      <Image
+                        source={{
+                          uri: profileInfo.profileImage,
+                        }}
+                        style={{
+                          width: 20,
+                          height: 20,
+                          borderRadius: 100,
+                          marginTop: -10,
+                        }}
+                      />
+                      <Image
+                        source={{
+                          uri: profileInfo.profileImage,
+                        }}
+                        style={{
+                          width: 20,
+                          height: 20,
+                          borderRadius: 100,
+                          marginTop: 10,
+                        }}
+                      />
+                      <Image
+                        source={{
+                          uri: profileInfo.profileImage,
+                        }}
+                        style={{
+                          width: 20,
+                          height: 20,
+                          borderRadius: 100,
+                          marginTop: -10,
+                        }}
+                      />
+                    </View>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    paddingHorizontal: 10,
+                    width: "80%",
+                    marginLeft: 10,
+                  }}
+                >
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "start",
+                      gap: 10,
+                      justifyContent: "space-between",
+                      //   backgroundColor: "blue",
+                    }}
+                  >
+                    <Text style={{ fontWeight: 700, fontSize: 18 }}>
+                      {profileInfo.username}
+                    </Text>
+                    <View style={{ flexDirection: "row", gap: 10 }}>
+                      <Text
+                        style={{ fontWeight: 700, fontSize: 18, color: "gray" }}
+                      >
+                        5m
+                      </Text>
+                      <EllipsisHorizontalIcon color={"gray"} />
+                    </View>
+                  </View>
+                  {/* Post Content */}
+                  <View
+                    style={{
+                      backgroundColor: "#fdfdfd",
+                      borderRadius: 10,
+                      //   paddingHorizontal: 10,
+                      paddingVertical: 10,
+                    }}
+                  >
+                    <Text style={{ fontWeight: "700", lineHeight: 23 }}>
+                      {data}
+                    </Text>
+                  </View>
+                  {/* Button */}
+                  <View
+                    style={{ marginTop: 10, flexDirection: "row", gap: 20 }}
+                  >
+                    <TouchableOpacity>
+                      <HeartIcon color={"black"} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                      <ChatBubbleOvalLeftIcon color={"black"} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                      <AntDesign name="retweet" size={24} color="black" />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                      <PaperAirplaneIcon color={"black"} />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            );
+          })}
         </View>
-        <View style={{ paddingHorizontal: 10, width: "80%", marginLeft: 10 }}>
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "start",
-              gap: 10,
-              justifyContent: "space-between",
-              //   backgroundColor: "blue",
-            }}
-          >
-            <Text style={{ fontWeight: 700, fontSize: 18 }}>meta</Text>
-            <View style={{ flexDirection: "row", gap: 10 }}>
-              <Text style={{ fontWeight: 700, fontSize: 18, color: "gray" }}>
-                5m
-              </Text>
-              <EllipsisHorizontalIcon color={"gray"} />
-            </View>
-          </View>
-          {/* Post Content */}
-          <View
-            style={{
-              backgroundColor: "#fdfdfd",
-              borderRadius: 10,
-              //   paddingHorizontal: 10,
-              paddingVertical: 10,
-            }}
-          >
-            <Text style={{ fontWeight: "500", lineHeight: 23 }}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-              praesentium velit in perspiciatis, quos quis tempora architecto
-              consequuntur veritatis quasi autem omnis harum corporis quisquam
-              dolorem labore quia mollitia officiis, excepturi molestiae cum.
-              Rem.
-            </Text>
-          </View>
-          {/* Button */}
-          <View style={{ marginTop: 10, flexDirection: "row", gap: 20 }}>
-            <TouchableOpacity>
-              <HeartIcon color={"black"} />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <ChatBubbleOvalLeftIcon color={"black"} />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <AntDesign name="retweet" size={24} color="black" />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <PaperAirplaneIcon color={"black"} />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    </View>
+      ) : null}
+    </>
   );
 }
 
