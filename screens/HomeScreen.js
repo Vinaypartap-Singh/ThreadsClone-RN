c35abc662fc1b034ae9f24a8ef5c982b257da74c
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-09-25 05:10:36 +0530

    Fetch post from firebase

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,61 @@
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  ActivityIndicator,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import HomePost from "../components/HomePost";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
+import { collection, getDocs, onSnapshot } from "firebase/firestore";
+import { themeColor } from "../theme/themeColors";
 
 export default function HomeScreen() {
-  return (
-    <ScrollView
-      contentInsetAdjustmentBehavior="automatic"
-      style={{ backgroundColor: "white", flex: 1 }}
-    >
-      <View style={{ marginVertical: 10, alignItems: "center" }}>
-        <Image
-          source={require("../assets/images/tLogo.png")}
-          style={{ width: 40, height: 40, objectFit: "contain" }}
-        />
-      </View>
+  const [threads, setThreads] = useState(null);
 
-      <HomePost />
-    </ScrollView>
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, "posts"), (snapshot) => {
+      const postsData = [];
+      snapshot.forEach((doc) => {
+        postsData.push({ id: doc.id, ...doc.data() });
+      });
+      setThreads(postsData);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  // console.log("Threads Data: ", threads);
+
+  return (
+    <>
+      {threads ? (
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={{ backgroundColor: "white", flex: 1 }}
+        >
+          <View style={{ marginVertical: 10, alignItems: "center" }}>
+            <Image
+              source={require("../assets/images/tLogo.png")}
+              style={{ width: 40, height: 40, objectFit: "contain" }}
+            />
+          </View>
+
+          {threads.map((data, index) => {
+            return <HomePost data={data} key={index} />;
+          })}
+        </ScrollView>
+      ) : (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator size={"large"} color={themeColor.primaryColor} />
+        </View>
+      )}
+    </>
   );
 }
 
